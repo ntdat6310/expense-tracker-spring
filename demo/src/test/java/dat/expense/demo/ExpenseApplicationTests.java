be538a9e763d724d581d5b5b4697a4e08efe1e21
Nguyễn Thành Đạt <thanhdat9096@gmail.com>
2023-07-09 14:26:53 +0700

    init project

---
+++ demo/src/test/java/dat/expense/demo/ExpenseApplicationTests.java
@@ -0,0 +1,13 @@
+package dat.expense.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
